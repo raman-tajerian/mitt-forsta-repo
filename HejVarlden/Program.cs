ae27b0d3fb0bd4aba4734d4052f9ce5fc5b536d8
Raman Tajerian <ramantajerian@gmail.com>
2026-09-03 16:22:58 +0200

    Add version 2 message

--- HejVarlden/Program.cs
+++ HejVarlden/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine($"Mitt namn: [Raman]");
 Console.WriteLine($"Datum: {DateTime.Now:2026-09-03}");
 Console.WriteLine("Mitt första Git-hanterade C#-projekt!");
+
+Console.WriteLine("Version 2 — nu kan jag Git!");
