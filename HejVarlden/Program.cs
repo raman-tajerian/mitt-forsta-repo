2c37fa32cfb378afca95b1e50b8159e2dd9ac1aa
Raman Tajerian <ramantajerian@gmail.com>
2026-09-03 16:13:24 +0200

    Initial commit: Hello World console app

---
+++ HejVarlden/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Hej världen!");
+Console.WriteLine($"Mitt namn: [Raman]");
+Console.WriteLine($"Datum: {DateTime.Now:2026-09-03}");
+Console.WriteLine("Mitt första Git-hanterade C#-projekt!");
